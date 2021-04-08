5f2328d2e2de1f9468bd78ce160a3c5776108841
BFTorres <bfonseca@gmx.de>
2021-04-08 12:00:27 +0200

    DEBUG ATTEMPT

--- game.js
+++ game.js
@@ -1,7 +1,8 @@
 
 let body = document.querySelector("body")
-let canvas;
+let canvas; 
 let ctx;
+
 //>>event listeners<<//
 document.addEventListener("keydown", function (even) {
   keys[even.code] = true;
@@ -14,25 +15,33 @@ let playerImg = new Image();
 playerImg.src = "./game-resources/assets/GUAR 3.png";
 
 let racerImg = new Image();
-racerImg.src = "./game-resources/assets/racer-removebg-preview.png"
+racerImg.src = "./game-resources/assets/daedra .png"
 
-let mudcrabImg = new Image ()
-mudcrabImg.src = './game-resources/assets/daedra .png'
+/*let mudcrabImg = new Image ()
+mudcrabImg.src = './game-resources/assets/daedra .png'*/
+
+
+
 //audio
 let splashScreenMusic = new Audio("./game-resources/audio/The Elder Scrolls 高齢者のスクロール.mp3");
 splashScreenMusic.loop = true;
 
-let gameMusic = new Audio("./game-resources/audio/Argonians Are Property (Dunmer Trap).mp3");
+/*let gameMusic = new Audio("./game-resources/audio/Argonians Are Property (Dunmer Trap).mp3");
+gameMusic.loop = true;*/
+
+let gameMusic = new Audio("./game-resources/audio/Dagoth Ur - Dagothwave.mp3");
 gameMusic.loop = true;
 
 let endGame = new Audio("./game-resources/audio/Oblivion-guard-MEME.mp3");
 
+
 //variables
-let muted;
+//let coins = [{x: 140, y: 0}]
+//let muteAudio;
 let score; 
 let scoreText;
-let highscore; 
-let highscoreText;
+/*let highscore; 
+let highscoreText;*/
 let player;
 let grav; 
 let enemies = [];
@@ -40,11 +49,41 @@ let gameSpeed;
 let keys = {};
 let isWasted = false;
 let gameOverScreen;
-let winGScreen; // inwork
+//let winGScreen;            // inwork
 let splashScreen;
 let canvasContainer;
 
+
 //>>classes<<//
+/*
+class Coins {
+  
+  // -----------coin------------
+  for (let i = 0; i < coins.length; i++) {
+    ctx.drawImage(coinImg, coins[i].x, coins[i].y)
+    coins[i].y++
+    if(coins[i].y + coinImg.height > playerY &&
+      coins[i].x < playerX + playerImg.width &&
+      coins[i].x + coinImg.width > playerX) {
+        score += 50
+        coins.splice(i, 1)
+        let coinAudio = new Audio('')
+        coinAudio.play()
+    }
+    if(coins[i].y == 100) {
+      coins.push({
+        x: Math.floor(Math.random()* (canvas.width - coinImg.width)),
+        y: 0
+      })
+    }
+  }
+
+  let coinImg = document.createElement('img')
+  coinImg.src = './'
+
+
+}*/
+
 
 
 class Player {
@@ -54,7 +93,7 @@ class Player {
     this.width = width;
     this.height = height;
     this.dirY = 0; 
-    this.jumpForce = 12; 
+    this.jumpForce = 10; 
     this.grounded = false;
     this.jumpTimer = 0;
   }
@@ -71,7 +110,7 @@ class Player {
     if (keys["Space"]) { 
       this.jump();
     } else {
-        this.jumpTimer = 2;  // 0 
+        this.jumpTimer = 1;  // 0 
       }
    this.y += this.dirY;
 
@@ -93,7 +132,7 @@ class Player {
       this.dirY = -this.jumpForce;
     } else if (this.jumpTimer > 0 && this.jumpTimer < 15) {       
         this.jumpTimer++;
-        this.dirY = -this.jumpForce - (this.jumpTimer / 50);
+        this.dirY = -this.jumpForce - (this.jumpTimer /50);
       }
     }
    draw(){ 
@@ -124,17 +163,6 @@ class Enemy {
   draw() {
     ctx.drawImage(racerImg, this.x, this.y, this.width, this.height)
   }
-////////////////////
-  /*let villianImages = [mudcrabImg, racerImg]
-  let lanes = [350, 430]
-  let villians = [
-    {
-        img: villianImages[Math.floor(Math.random() * villianImages.length)],
-        x: canvas.width,
-        y: lanes[Math.floor(Math.random() * lanes.length-20)]
-    }
-]*/
-//////////////////////
 }
 //score
 class Statistics {
@@ -149,7 +177,7 @@ class Statistics {
   draw() {
     ctx.beginPath();
     ctx.fillStyle = this.colour;
-    ctx.font = this.size +"px";
+    ctx.font = this.size +"px Planewalker, arial";
     ctx.textAlign = this.alignment
     ctx.fillText(this.text, this.x, this.y);
     ctx.closePath();
@@ -162,12 +190,13 @@ class Statistics {
 
 //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 function createEnemies(){ 
-  let size = randomRange(60, 170);// player = new Player(180, canvas.height, 160, 220, "#ff7575");
+  let size = randomRange(70, 150);
+  // player = new Player(180, canvas.height, 160, 220, "#ff7575");
   let type = randomRange(0, 1);
   let enemy = new Enemy(canvas.width + size, canvas.height - size, size, size, '#bf1313');
  
   if (type == 1) {
-        enemy.y -= player.originalHeight - 15;
+        enemy.y -= player.originalHeight - 10;
       }
       enemies.push(enemy);
 
@@ -177,7 +206,7 @@ function randomRange(min, max) {
     }
  }
 //timer
-let initialSpawnTimer = 150;
+let initialSpawnTimer = 200;
 let spawnTimer = initialSpawnTimer;
 //splash
 function splash(){
@@ -218,11 +247,11 @@ function startGame(){
   canvas.width = window.innerWidth; 
   canvas.height = window.innerHeight;
   //basics
-  ctx.font = "20px"; 
+  ctx.font = "20px Planewalker, arial"; 
   gameSpeed = 3; 
   grav = 1; 
   score = 0; 
-  highscore = 1000; //0
+  highscore = 0; //0
   // N'WAH Stats
   player = new Player(180, canvas.height, 160, 220, "#ff7575");
   // Score Stats
@@ -232,7 +261,7 @@ function startGame(){
 }
 /////////////////////inwork
 //Wasted Screen
-function win(){
+/*function win(){
   winG.currentTime = 0
   winG.play();
   winG.volume = 0.30;
@@ -254,9 +283,9 @@ body.appendChild(winGScreen)
         gameMusic.play()
         newGame();
   })  
-}
+}*/
 
-//Win screen
+
 function gameOver(){
     endGame.currentTime = 0
     endGame.play();
@@ -295,32 +324,9 @@ function newGame() {
     initialSpawnTimer = 200;
     spawnTimer = 100;
   startGame();
-  //////////////////////////////
-  mute();
-  let muteBtn = document.querySelector("#mute");
-  if (muted) {
-    game.music.pause();
-    muteBtn.innerHTML = `<img src ="./img/soundOff.png" alt="Sound Off"/>`;
-  } else {
-    game.music.play();
-    muteBtn.innerHTML = `<img src ="./img/soundOn.png" alt="Sound On"/>`;
-  };
-  ///////////////////////////////
 }
 
-const mute = () => {
-    let muteBtn = document.querySelector("#mute");
-    muteBtn.addEventListener("click", () => {
-      if (muted) {
-        game.music.play();
-        muteBtn.innerHTML = `<img src ="./img/soundOn.png" alt="Sound On"/>`;
-      } else {
-        game.music.pause();
-        muteBtn.innerHTML = `<img src ="./img/soundOff.png" alt="Sound Off"/>`;
-      }
-      muted = !muted;
-    });
-  };
+
 /////////////////////////////////////////////////
 
 
@@ -332,7 +338,7 @@ function updateGame() {
   if (spawnTimer <= 0) {
     createEnemies();
     spawnTimer = initialSpawnTimer - gameSpeed * 50; 
-    console.log(gameSpeed)
+    //console.log(gameSpeed)
     if (spawnTimer < 100) {
       spawnTimer = 100;
     }
@@ -340,6 +346,7 @@ function updateGame() {
 //npc crt
 for (let i = 0; i < enemies.length; i ++) { 
   let e = enemies[i];
+
   //collision mech
   if (e.x + e.width < 0) {
     enemies.splice(i, 1);
@@ -354,7 +361,7 @@ for (let i = 0; i < enemies.length; i ++) {
       gameMusic.currentTime = 0  
       enemies = []; 
       spawnTimer = initialSpawnTimer;
-      gameSpeed = 2;
+      gameSpeed = 1;
       isWasted = true
       gameOver();     
   }
@@ -362,7 +369,7 @@ for (let i = 0; i < enemies.length; i ++) {
   console.log("game continues")
 }
   player.animation();
-  gameSpeed += 0.010; 
+  gameSpeed += 0.040; 
   score ++;
   scoreText.text = "Score: " + score;
   scoreText.draw();
